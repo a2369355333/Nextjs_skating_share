a52ad9f473ef71f1fca68c57b98b8ab2b7480ddc
Oreo <a2369355333@yahoo.com.tw>
2025-12-25 22:37:15 +0800

    add suspens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import Image from "next/image";
 import Layout from "@/component/layout";
 import Content from "@/modules/home/content";
+import { Suspense } from "react";
+import { Loader2 } from "lucide-react";
 
 export default function Home() {
   return (
     <Layout>
-      <Content/>
+      <Suspense fallback={
+        <div className="flex items-center justify-center py-20">
+          <Loader2 className="w-12 h-12 text-rose-400 animate-spin" />
+        </div>
+      }>
+        <Content />
+      </Suspense>
     </Layout>
   );
 }
